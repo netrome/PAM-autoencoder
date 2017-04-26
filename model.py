c3b898b1c0eded569fa60fa52d68d178877e4a3d
Patrik Barkman <barkm@kth.se>
2017-04-26 16:14:30 +0200

    Model skeleton

---
+++ model.py
@@ -0,0 +1,43 @@
+class Autoencoder:
+    """ Autoencoder class
+    """
+    
+    def __init__(self):
+        """ Sets hyper-parameters
+        """
+        pass
+
+    
+    def build_model(self):
+        """ Builds model graph
+        """
+        pass
+
+ 
+    def train(self):
+        """ Builds training graph
+        """
+        pass
+
+    
+    def encoder(self):
+        """ Builds encoder graph
+        """
+        pass
+
+
+    def decoder(self):
+        """ Bulds decoder graph
+        """
+        pass
+
+    def save(self):
+        """ Saves tensorflow graph
+        """
+        pass
+
+
+    def load(self):
+        """ Loads tensorflow graph
+        """
+        pass
