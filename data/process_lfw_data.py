3e766e0940d686061061bf24de4ff82586a61ccb
Mårten Nilsson <nilssonmartenx@gmail.com>
2017-04-26 16:33:47 +0200

    Data processing implemented

--- data/process_lfw_data.py
+++ data/process_lfw_data.py
@@ -3,17 +3,36 @@ import glob
 import sys
 import os
 
+# Get globals
+CENTER_BLACK = "center" in sys.argv
+
+# define paths
 place = os.getcwd()
 lfw_path = place + "/lfw-deepfunneled/"
-out_path = place + "/small_pics/"
+pattern_path = place + "/patterns/"
+target_path = place + "/targets/"
+
+# Clear directories
+os.system("rm targets/*")
+os.system("rm patterns/*")
 
 k = 0
-n = 100000
+n = 100
 for path, dirs, files in os.walk(lfw_path):
     if len(files) > 0 and k < n:
         for i in files:
             image_path = path + "/" + i
-            os.system("convert -resize 64x64 {0} {1}out{2}.jpg".format(image_path, out_path, k))
 
+            # Save cropped original image
+            os.system("convert -resize 64x64 {0} /tmp/img.jpg".format(image_path))
+
+            os.system("convert /tmp/img.jpg {0}target{1}.jpg".format(target_path, k))
+            os.system("convert /tmp/img.jpg {0}pattern{1}.jpg".format(pattern_path, k))
             k += 1
+
+            if CENTER_BLACK:
+                os.system("convert /tmp/img.jpg {0}target{1}.jpg".format(target_path, k))
+                os.system("convert -stroke black -draw \"rectangle 24, 24, 40, 40\" /tmp/img.jpg {0}pattern{1}.jpg".format(pattern_path, k))
+                k += 1
+                
 
