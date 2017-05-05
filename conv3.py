da117879c1453fd9c3bf7b3075ef2d794bf22c4d
Mårten Nilsson <nilssonmartenx@gmail.com>
2017-05-05 14:54:14 +0200

    SOMETHING WORKS! (model conv3 with sigmoid at end)

--- conv3.py
+++ conv3.py
@@ -32,7 +32,7 @@ class ConvEncoder3:
         """ Builds training graph
         """
         err = tf.reduce_mean(tf.abs(self.decoder - self.images))
-        train_step = tf.train.AdamOptimizer().minimize(err, name="train_step")
+        train_step = tf.train.AdamOptimizer(0.001).minimize(err, name="train_step")
 
         # Add summary scalar for tensor board
         tf.summary.scalar("reduced_abs_err", err)
@@ -44,33 +44,54 @@ class ConvEncoder3:
         """
         
         # First convolutional layer
-        W1 = tf.Variable(tf.truncated_normal([3, 3, 3, 4]))
+        W1 = tf.Variable(tf.truncated_normal([7, 7, 3, 24], stddev=0.01))
         x1 = tf.nn.conv2d(images, W1, [1, 3, 3, 1], padding="SAME")
         h1 = tf.nn.relu(x1)
 
         # Second conv layer
-        W2 = tf.Variable(tf.truncated_normal([7, 7, 4, 8]))
+        W2 = tf.Variable(tf.truncated_normal([5, 5, 24, 48], stddev=0.01))
         x2 = tf.nn.conv2d(h1, W2, [1, 2, 2, 1], padding="SAME")
-        h2 = tf.nn.relu(x2, name="bottleneck")
+        h2 = tf.nn.relu(x2)
 
+        # Third convolutional layer
+        W3 = tf.Variable(tf.truncated_normal([3, 3, 48, 48], stddev=0.01))
+        x3 = tf.nn.conv2d(h2, W3, [1, 1, 1, 1], padding="SAME")
+        h3 = x3 #tf.nn.relu(x1)
+
+        # Forth conv layer
+        W4 = tf.Variable(tf.truncated_normal([3, 3, 48, 24], stddev=0.01))
+        x4 = tf.nn.conv2d(h3, W4, [1, 1, 1, 1], padding="SAME")
+        h4 = tf.nn.relu(x4, name="bottleneck") 
+        
         # Save shapes
-        self.shapes += [tf.shape(images), tf.shape(x1)]
+        self.shapes += [tf.shape(images), tf.shape(x1), tf.shape(x2), tf.shape(x3)]
 
-        return h2
+        return h4
 
 
     def decoder(self, bottleneck):
         """ Builds decoder graph
         """
+
         # First deconv
-        W2 = tf.Variable(tf.truncated_normal([7, 7, 4, 8]))
-        x2 = tf.nn.conv2d_transpose(bottleneck, W2, self.shapes[1], [1, 2, 2, 1])
+        W4 = tf.Variable(tf.truncated_normal([3, 3, 48, 24], stddev=0.01))
+        x4 = tf.nn.conv2d_transpose(bottleneck, W4, self.shapes[-1], [1, 1, 1, 1])
+        h4 = tf.nn.relu(x4)
+
+        # Second deconv
+        W3 = tf.Variable(tf.truncated_normal([3, 3, 48, 48], stddev=0.01))
+        x3 = tf.nn.conv2d_transpose(h4, W3, self.shapes[-2], [1, 1, 1, 1])
+        h3 = tf.nn.relu(x3)
+
+        # First deconv
+        W2 = tf.Variable(tf.truncated_normal([5, 5, 24, 48], stddev=0.01))
+        x2 = tf.nn.conv2d_transpose(h3, W2, self.shapes[1], [1, 2, 2, 1])
         h2 = tf.nn.relu(x2)
 
         # Second deconv
-        W1 = tf.Variable(tf.truncated_normal([7, 7, 3, 4]))
-        x1 = tf.nn.conv2d_transpose(h2, W1, self.shapes[0], [1, 2, 2, 1])
-        h1 = tf.nn.relu(x1, name="raw_out")
+        W1 = tf.Variable(tf.truncated_normal([7, 7, 3, 24], stddev=0.01))
+        x1 = tf.nn.conv2d_transpose(h2, W1, self.shapes[0], [1, 3, 3, 1])
+        h1 = tf.nn.sigmoid(x1, name="raw_out")
        
 
         return h1 
