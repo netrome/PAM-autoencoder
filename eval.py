76b32a36bd37e0f82b7a683e277816a20cc21d18
Patrik Barkman <barkm@kth.se>
2017-05-19 12:19:59 +0200

    New eval script for test data

--- eval.py
+++ eval.py
@@ -13,9 +13,9 @@ import sys
 
 np_data = np.load(sys.argv[1])
 
-print(np_data)
-
 # create autoencoder
+#ae = ConvEncoder3()
+#ae = Autoencoder()
 ae = FullSkip()
 ae.build_model()
 ae.train()
@@ -32,22 +32,32 @@ print()
 print("------------------------------")
 
 # Test the model
-idx = [0, 1, 2, 3, 4, -1, -2, -3, -4]
+np.random.seed(42)
+idx = np.random.randint(1100, 1199, 8)
 images = np_data["patterns"][idx]
 out = sess.run("raw_out:0", feed_dict={"raw_data:0": images})
 
 # Plot some samples
 
-plt.figure()
+fig = plt.figure(figsize=(13.3,5))
 
 for i in range(out.shape[0]):
-    #pueh = (out[i] - np.min(out[i])) / (np.max(out[i]) - np.min(out[i]) + 1E-6)
     plt.subplot(3, out.shape[0], i + 1)
     plt.imshow(out[i])
+    frame = plt.gca()
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
     plt.subplot(3, out.shape[0], i + out.shape[0] + 1)
     plt.imshow(np_data["patterns"][idx[i]]/255)
+    frame = plt.gca()
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
     plt.subplot(3, out.shape[0], i + out.shape[0]*2 + 1)
     plt.imshow(np_data["targets"][idx[i]]/255)
-    #print("Scaled with value: ", np.max(out[i]) - np.min(out[i]) + 1E-6)
+    frame = plt.gca()
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
 
+fig.tight_layout()
 plt.show()
+#plt.savefig("faces.jpeg", format="jpeg", dpi=300)
